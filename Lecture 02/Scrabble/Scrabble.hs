a4206d103d1f8d3aa2c1f76c988be88be81936bf
Michael Mandl <mandlm@molez.net>
2016-10-15 16:04:27 +0200

    Implemented exercise 3: wordFitsTemplate

--- Lecture 02/Scrabble/Scrabble.hs
+++ Lecture 02/Scrabble/Scrabble.hs
@@ -10,13 +10,33 @@ formableBy [] _ = True
 formableBy (x:xs) hand = let remainingHand = delete x hand in
   elem x hand && formableBy xs remainingHand
  
-wordsFrom :: Hand -> [String]
-wordsFrom hand = wordsFromList hand allWords
-
 wordsFromList :: Hand -> [String] -> [String]
 wordsFromList hand [] = []
 wordsFromList hand (x:[]) 
   | formableBy x hand = [x]
   | otherwise = []
 wordsFromList hand (x:xs) = wordsFromList hand [x] ++ wordsFromList hand xs
+
+wordsFrom :: Hand -> [String]
+wordsFrom hand = wordsFromList hand allWords
+
+type Template = String
+
+wordMatchesTemplate :: String -> Template -> Bool
+wordMatchesTemplate [] [] = True
+wordMatchesTemplate (w:ws) (t:ts)
+  | t == '?' = wordMatchesTemplate ws ts
+  | otherwise = w == t && wordMatchesTemplate ws ts
+wordMatchesTemplate _ _ = False
+
+removeTemplateChars :: String -> Template -> String
+removeTemplateChars word [] = word
+removeTemplateChars word (t:ts)
+  | t == '?' = removeTemplateChars word ts
+  | otherwise = removeTemplateChars (delete t word) ts
+
+wordFitsTemplate :: Template -> Hand -> String -> Bool
+wordFitsTemplate template hand word = let matchWord = removeTemplateChars word template in 
+  formableBy matchWord hand && wordMatchesTemplate word template
+
 
